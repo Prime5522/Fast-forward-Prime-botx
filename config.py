6b2eb4b289553ce3c2065892034f0ee7e010060b
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-03 18:22:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
+import re
 from os import environ 
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002245813234').split()] 
+# give channel id with separate space. Ex: ('-10073828 -102782829 -1007282828')
+
 
 class Config:
     API_ID = int(environ.get("API_ID", ""))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Primebotz") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Prime-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 class temp(object): 
     lock = {}
@@ -24,6 +26,3 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
