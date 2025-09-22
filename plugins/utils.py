da683ef5f3b63e8e781d31a92255f3ec4c32b7d0
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-09-22 07:54:39 +0600

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 import time as tm
 from database import Db, db
@@ -8,9 +5,7 @@ from .test import parse_buttons
 
 STATUS = {}
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 class STS:
     def __init__(self, id):
@@ -63,6 +58,4 @@ class STS:
         return bot, configs['caption'], configs['forward_tag'], {'filters': filters,
                 'keywords': configs['keywords'], 'min_size': min, 'max_size': max, 'extensions': configs['extension'], 'skip_duplicate': duplicate, 'db_uri': configs['db_uri']}, configs['protect'], button
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
