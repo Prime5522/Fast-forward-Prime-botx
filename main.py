d673203e902120adc95ba5eaf006a9ed0aa1d278
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-09-22 07:49:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 import asyncio, logging
 from config import Config
@@ -9,9 +7,7 @@ from typing import Union, Optional, AsyncGenerator
 from logging.handlers import RotatingFileHandler
 from plugins.regix import restart_forwards
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 if __name__ == "__main__":
     VJBot = VJ(
@@ -70,6 +66,5 @@ if __name__ == "__main__":
 
     asyncio.get_event_loop().run_until_complete(main())
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
+
