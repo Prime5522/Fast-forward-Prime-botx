d4880d1f231fde15101d2a5c0fe9da96d903d97b
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-09-22 11:00:02 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] 
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002245813234').split()] 
 # give channel id with separate space. Ex: ('-10073828 -102782829 -1007282828')
 
 
@@ -12,7 +12,7 @@ class Config:
     API_ID = int(environ.get("API_ID", ""))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "PrimeXBots") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Primebotz") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Prime-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
